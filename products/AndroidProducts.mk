bf4e2cbb5156dbc946408b313fb9a5d77ed6d5cf
teemodk <teemodk@gmail.com>
2013-09-16 22:16:46 +0200

    Add endeavoru

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -121,3 +121,6 @@ endif
 ifeq (pa_geespr,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_geespr.mk
 endif
+ifeq (pa_endeavoru,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_endeavoru.mk
+endif
